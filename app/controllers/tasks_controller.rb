0515299928cd14d4d5a1317f677b79034be05da8
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-09 12:48:57 +0000

    Add new, show, index routes for task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,23 @@
 class TasksController < ApplicationController
   # Execute find_task before specific CRUD actions
-  before_action :find_task, only[:show, :edit, :update, :destroy]
-  # Read all tasks
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # Read all tasks (4)
   def index
+    @tasks = Task.all
   end
-  # Create a new task
+  # Create a new task (5)
   def new
+    @task = Task.new
   end
   def create
+    @task = Task.create(task_params)
+    # Add redirect after submiting the form
+    redirect_to task_path(@task)
   end
-  # Read a task
+  # Read a task (4)
   def show
+
   end
   # Update a task
   def edit
@@ -23,7 +30,7 @@ class TasksController < ApplicationController
 
   # Private methods
   # Find the restaurant based on the id parameter
-  def find_task
-
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   # Read all tasks
   get '/tasks', to: 'tasks#index'
   # Create a new task
-  get '/tasks/new', to: 'tasks#new' # Form
+  get '/tasks/new', to: 'tasks#new', as: :new_task # Form
   post '/tasks', to: 'tasks#create'
   # Read a task
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show', as: :task
   # Update a task
-  get '/tasks/:id/edit', to: 'tasks#edit' # Form
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task # Form
   patch '/tasks/:id', to: 'tasks#update'
   # Delete a task
   delete '/tasks/:id', to: 'tasks#destroy'
