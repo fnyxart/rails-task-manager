49870f14198e19c766d04ca0f16dd37b0c7b7656
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-09 13:08:38 +0000

    Add update route to task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,20 +17,29 @@ class TasksController < ApplicationController
   end
   # Read a task (4)
   def show
-
   end
-  # Update a task
+  # Update a task (6)
   def edit
   end
   def update
+    @task.update(task_params)
+    # Add redirect after submiting the form
+    redirect_to task_path(@task)
   end
-  # Delete a task
+  # Delete a task (7)
   def destroy
+    @task.destroy
+    # Add redirect to show page after submiting the form
+    redirect_to tasks_path
   end
 
   # Private methods
   # Find the restaurant based on the id parameter
   def set_task
     @task = Task.find(params[:id])
+  end
+  # Get task parameters
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
